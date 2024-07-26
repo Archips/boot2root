59093f93440fa73a9b870c65701ac4d68c5b95c0
athirion <athirion@scaleway.com>
2024-07-26 10:14:32 +0200

    add scripts

---
+++ scripts/turtle.py
@@ -0,0 +1,57 @@
+import turtle
+t = turtle.Turtle()
+t.speed(5) # 1:slowest, 3:slow, 5:normal, 10:fast, 0:fastest
+t.forward(100)
+t.left(90)
+t.forward(50)
+i = 0
+# 2
+while i < 180:
+  t.forward(1)
+  t.left(1)
+  i = i + 1
+#363
+i = 0
+while i < 180:
+  t.forward(1)
+  t.right(1)
+  i += 1
+#723
+t.forward(50)
+#725
+t.forward(210)
+t.backward(210)
+t.right(90)
+t.forward(120)
+#730
+t.right(10)
+t.forward(200)
+t.right(150)
+t.forward(200)
+t.backward(100)
+t.right(120)
+t.forward(50)
+#738
+t.left(90)
+t.forward(50)
+#740
+i = 0
+while i < 180:
+  t.forward(1)
+  t.left(1)
+  i += 1
+#1100
+i = 0
+while i < 180:
+  t.forward(1)
+  t.right(1)
+  i += 1
+#1462
+t.forward(100)
+t.backward(200)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.backward(200)
